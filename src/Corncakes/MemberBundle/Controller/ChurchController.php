fd8f9a7be67a04a27e032a6cf0d701ca5694e9d3
Corncakes <corncakes@gmail.com>
2016-03-01 21:07:30 +0000

    Church form and save in the db

--- src/Corncakes/MemberBundle/Controller/ChurchController.php
+++ src/Corncakes/MemberBundle/Controller/ChurchController.php
@@ -3,7 +3,9 @@
 namespace Corncakes\MemberBundle\Controller;
 
 use Corncakes\MemberBundle\Entity\Church;
+use Corncakes\MemberBundle\Form\ChurchType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ChurchController extends Controller
 {
@@ -18,29 +20,50 @@ class ChurchController extends Controller
         ));
     }
 
-    public function createAction()
+    public function createAction(Request $request)
     {
-        $church = new Church();
+        $entity = new Church();
+        $form = $this->createCreateForm($entity);
 
-        $church->setName('Bonanza');
-        $church->setAddress('Barrio Bonanza Turbaco');
+        $form->handleRequest($request);
+
+        if($form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('church'));
+        }
+
+        //$church->setName('Bonanza');
+        //$church->setAddress('Barrio Bonanza Turbaco');
         //$church->setPhone('');
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($church);
-
-        $em->flush();
-
-        return $this->render('CorncakesMemberBundle:Church:create.html.twig', array(
+        return $this->render('CorncakesMemberBundle:Church:new.html.twig', array(
             // ...
         ));
     }
 
     public function newAction()
     {
+        $entity = new Church();
+        $form = $this->createCreateForm($entity);
+
         return $this->render('CorncakesMemberBundle:Church:new.html.twig', array(
-            // ...
+            'form'   => $form->createView(),
         ));
+    }
+
+    public function createCreateForm(Church $entity)
+    {
+        $form = $this->createForm(ChurchType::class, $entity, array(
+            'action' => $this->generateUrl('church_create'),
+            'method' => 'POST',
+        ));
+
+        return $form;
     }
 
     public function showAction($id)
---
+++ src/Corncakes/MemberBundle/Form/ChurchType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Corncakes\MemberBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ChurchType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('address')
+            ->add('phone')
+            ->add('save', SubmitType::class, array(
+                'label' => 'Guardar',
+                'attr' => array('class' => 'button'),
+            ));
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Corncakes\MemberBundle\Entity\Church'
+        ));
+    }
+}
